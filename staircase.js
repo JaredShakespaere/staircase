7f965abcd266321fd0e0709362ca36d1c1aaa66f
JaredShakespaere <90650932+JaredShakespaere@users.noreply.github.com>
2022-04-15 08:25:03 -0500

    created a loop over the array, but the loop length isnt working

--- staircase.js
+++ staircase.js
@@ -2,5 +2,10 @@ function staircase(n) {
 	let arrLength = n;
 	const arr = new Array(n);
 	console.log(arr);
+
+    for(let i = 0; i < arr.length -1 ; i++){
+        
+        console.log(arr.push('#'))
+    }
 	
 }
